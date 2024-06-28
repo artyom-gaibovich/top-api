98a3fb966526601acc40c37e9d26d7204b2ecd09
ArtyomIzmaylov <artyomguybov2002@gmail.com>
2024-06-28 03:02:36 +0300

    task is completed

--- src/top-page/top-page.model.ts
+++ src/top-page/top-page.model.ts
@@ -1,28 +1,65 @@
+import {TimeStamps, Base} from "@typegoose/typegoose/lib/defaultClasses";
+import {prop} from "@typegoose/typegoose";
+
 export enum TopLevelCategory {
-    Courses,
-    Services,
-    Books,
-    Products,
+    Courses= 0,
+    Services = 1,
+    Books= 2,
+    Products = 3,
 
 }
-export class TopPageModel {
-    _id : string
-    firstCategory : TopLevelCategory;
-    secondCategory : string;
+export interface TopPageModel extends Base {}
+
+
+export class HhData {
+    @prop()
+    count : number;
+
+    @prop()
+    juniorSalary : number;
+
+    @prop()
+    middleSalary : number;
+
+    @prop()
+    seniorSalary : number;
+}
+export class TopPageAdvantage {
+    @prop()
     title : string;
+
+    @prop()
+    description : string
+}
+
+export class TopPageModel extends TimeStamps{
+
+    @prop({enum : TopLevelCategory, type : () => Number})
+    firstCategory : TopLevelCategory;
+
+    @prop()
+    secondCategory : string;
+
+    @prop()
+    title : string;
+
+    @prop()
     category : string;
-    hh? : {
-        count : number;
-        juniorSalary : number;
-        middleSalary : number;
-        seniorSalary : number;
-    }
-    advantages : {
-        title : string;
-        description : string
-    }
+
+    @prop()
+    hh? : HhData
+
+
+    @prop({type : () => [TopPageAdvantage]})
+    advantages : TopPageAdvantage[]
+
+    @prop()
     seoText : string
+
+    @prop()
     tagsTitle : string
+
+    @prop({type : () => [String]})
     tags : string[]
 
 }
